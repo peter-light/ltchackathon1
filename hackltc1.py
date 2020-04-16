4394515bdc0c17978189a0aeb174681ff5acf7a0
Peter <oluwaseyidave7@gmail.com>
2020-04-16 09:14:21 +0100

    first commit

---
+++ hackltc1.py
@@ -0,0 +1,7 @@
+
+
+for num in range(1,15):
+    print(num)
+    if num==2:
+        break
+
---
+++ hackltc2.py
@@ -0,0 +1,12 @@
+num = int(input("Type in a number: "))
+if num % 3 == 0 and num % 5 == 0:
+    print("FizzBuzz")
+elif num % 3 == 0:
+    print("Fizz")
+elif num % 5 == 0:
+    print("Buzz")
+else:
+    print(num)
+
+
+
